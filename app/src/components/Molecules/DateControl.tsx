4a85fc19ccf2ebcd68b6422d7e16d7f08d1f6b68
DIO0550 <diot12345@yahoo.co.jp>
2022-04-26 23:07:35 +0900

    ボタンをsvgに変更

--- app/src/components/Molecules/DateControl.tsx
+++ app/src/components/Molecules/DateControl.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import useBooksDate from '../../hooks/useBooksDate';
 
 import dateControlStyle from './DateControl.module.scss';
 
-const DateControlComponent: React.VFC = () => {
+const DateControlComponent: React.FC = () => {
   const { dateStr, incrementMonth, decrementMonth } = useBooksDate();
   const date = new Date(dateStr);
   const dateBlockValue = `${date.getFullYear()}年${date.getMonth() + 1}月`;
@@ -14,20 +15,20 @@ const DateControlComponent: React.VFC = () => {
         <div className={dateControlStyle['date-button-block']}>
           <button
             type="button"
-            className={`${dateControlStyle['date-button']} ${dateControlStyle.left}`}
+            className={`${dateControlStyle['date-button']}`}
             onClick={decrementMonth}
           >
-            &lt;
+            <div className={`${dateControlStyle.left}`} />
           </button>
         </div>
         <div className={dateControlStyle['date-block']}>{dateBlockValue}</div>
         <div className={dateControlStyle['date-button-block']}>
           <button
             type="button"
-            className={`${dateControlStyle['date-button']} ${dateControlStyle.right}`}
+            className={`${dateControlStyle['date-button']}`}
             onClick={incrementMonth}
           >
-            &gt;
+            <div className={`${dateControlStyle.right}`} />
           </button>
         </div>
       </div>
