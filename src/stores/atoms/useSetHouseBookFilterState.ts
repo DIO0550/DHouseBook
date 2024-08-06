42fe33f64e834a78cb23d737fc945ad372ed7258
DIO0550 <diot12345@yahoo.co.jp>
2024-08-06 23:31:11 +0900

    フィルター実装

--- src/stores/atoms/useSetHouseBookFilterState.ts
+++ src/stores/atoms/useSetHouseBookFilterState.ts
@@ -1,9 +1,40 @@
 import { useRecoilCallback } from 'recoil';
+import { HouseBookItemCategory } from '@/utils/editors/houseBookItemCategory';
 import {
   HouseBookFilter,
+  HouseBookFilterName,
+  HouseBookFilterNameCondition,
   HouseBookFilterOperationDefault,
   houseBookFilterState,
 } from './houseBookFilterState';
+
+const UpdateType = {
+  Category: 'Category',
+  Value: 'Value',
+  Condition: 'Condition',
+} as const;
+type UpdateType = (typeof UpdateType)[keyof typeof UpdateType];
+
+type UpdateFilterNameCategory = {
+  type: typeof UpdateType.Category;
+  value: HouseBookItemCategory;
+};
+
+type UpdateFilterNameValue = {
+  type: typeof UpdateType.Value;
+  value: HouseBookFilterName['value'];
+};
+
+type UpdateFilterNameCondition = {
+  type: typeof UpdateType.Value;
+  value: HouseBookFilterNameCondition;
+};
+
+type UpdateFilterName =
+  | { category: typeof HouseBookItemCategory.Name }
+  | UpdateFilterNameCategory
+  | UpdateFilterNameValue
+  | UpdateFilterNameCondition;
 
 const useSetHouseBookFilterState = () => {
   const setHouseBookFilterWithCategory = useRecoilCallback(
@@ -25,6 +56,25 @@ const useSetHouseBookFilterState = () => {
               ? filter.operation
               : HouseBookFilterOperationDefault,
           };
+
+          return [...current, newValue];
+        });
+      },
+    [],
+  );
+
+  const updateHouseBookFilter = useRecoilCallback(
+    ({ set }) =>
+      (id: string, type: UpdateType, value: string) => {
+        set(houseBookFilterState, (current) => {
+          if (!current) {
+            return undefined;
+          }
+
+          const filter: HouseBookFilter = current.find((f) => f.id === id);
+          if (!filter) {
+            return current;
+          }
 
           return [...current, newValue];
         });
