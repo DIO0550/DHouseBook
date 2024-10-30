06649c60663621c2299bdfcf94587b3ded3332aa
DIO0550 <diot12345@yahoo.co.jp>
2024-10-30 20:27:17 +0900

    デザイン調整

--- src/features/editors/components/editors/HouseBookEditor/HouseBookEditor.tsx
+++ src/features/editors/components/editors/HouseBookEditor/HouseBookEditor.tsx
@@ -5,6 +5,7 @@ import { useHouseBookEditor } from '@/features/editors/hooks/useHouseBookEditor'
 import { usePurchasedItemSelect } from '@/features/editors/hooks/usePurchasedItemSelect';
 import { useHouseBookEditorMode } from '@/features/editors/hooks/useHouseBookEditorMode';
 import { HouseBookEditorNavigation } from '../HouseBookEditorNavigation';
+import styles from './HouseBookEditor.module.scss';
 
 const HouseBookEditor = memo(({ fileId }: { fileId: string }) => {
   const editor = useHouseBookEditor({ fileId });
@@ -12,21 +13,25 @@ const HouseBookEditor = memo(({ fileId }: { fileId: string }) => {
   const { mode, changeMode } = useHouseBookEditorMode();
 
   return (
-    <div>
-      <HouseBookEditorNavigation
-        mode={mode}
-        date={editor.houseBookDate}
-        onChangeMode={changeMode}
-        onAddItem={() => editor.addPurchasedItem(HouseBookItem.init())}
-        onChangeDate={editor.updateHouseBookDate}
-      />
-      <PurchasedItemList
-        mode={mode}
-        purchasedItems={editor.filteredHouseBookItems}
-        onUpdateData={editor.updatePurchasedItem}
-        selectedItems={selectItemIds}
-        onChangeSelect={changeSelectItem}
-      />
+    <div className={`${styles['editor-container']}`}>
+      <div>
+        <HouseBookEditorNavigation
+          mode={mode}
+          date={editor.houseBookDate}
+          onChangeMode={changeMode}
+          onAddItem={() => editor.addPurchasedItem(HouseBookItem.init())}
+          onChangeDate={editor.updateHouseBookDate}
+        />
+      </div>
+      <div className={`${styles['editor-list']}`}>
+        <PurchasedItemList
+          mode={mode}
+          purchasedItems={editor.filteredHouseBookItems}
+          onUpdateData={editor.updatePurchasedItem}
+          selectedItems={selectItemIds}
+          onChangeSelect={changeSelectItem}
+        />
+      </div>
     </div>
   );
 });
