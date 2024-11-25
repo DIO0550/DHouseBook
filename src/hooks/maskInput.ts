d90b91b0d597f219ceaedb98ed062b93fc83fbe6
DIO0550 <diot12345@yahoo.co.jp>
2024-11-26 00:12:54 +0900

    マスクインプット

--- src/hooks/maskInput.ts
+++ src/hooks/maskInput.ts
@@ -10,7 +10,22 @@ const RegExpMap: { [key in string]: RegExp } = {
 };
 
 const MaskRegExp = {
-  from: (value: string) => {},
+  from: (value: string) => {
+    const characters = Array.from(value);
+    const result = characters.map((char) => {
+      if (Object.hasOwn(RegExpMap, char)) {
+        return RegExpMap[char];
+      }
+
+      return char;
+    });
+
+    const maskRegExp: MaskRegExp = {
+      value: result,
+    };
+
+    return maskRegExp;
+  },
 } as const;
 
 const maskInput = (
